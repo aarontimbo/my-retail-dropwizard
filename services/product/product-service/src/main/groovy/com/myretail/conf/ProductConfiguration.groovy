62c01502b24e8defc8419e801010540463ed260f
Aaron Timbo <aaron.timbo@gmail.com>
2016-02-24 22:42:50 -0600

    Add configuration for mongo DB. Add managed mongo DB connection to the app environment.

--- services/product/product-service/src/main/groovy/com/myretail/conf/ProductConfiguration.groovy
+++ services/product/product-service/src/main/groovy/com/myretail/conf/ProductConfiguration.groovy
@@ -1,10 +1,26 @@
 package com.myretail.conf
 
+import com.fasterxml.jackson.annotation.JsonProperty
 import io.dropwizard.Configuration
+import org.hibernate.validator.constraints.NotEmpty
+
+import javax.validation.constraints.Max
+import javax.validation.constraints.Min
 
 /**
  * Product Service configuration
  */
 class ProductConfiguration extends Configuration {
+    @JsonProperty
+    @NotEmpty
+    public String mongohost = "localhost";
 
+    @JsonProperty
+    @Min(1L)
+    @Max(65535L)
+    public int mongoport = 27017;
+
+    @JsonProperty
+    @NotEmpty
+    public String mongodb = "myretail";
 }
